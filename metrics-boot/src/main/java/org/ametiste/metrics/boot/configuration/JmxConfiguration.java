b16128565664a1aa67148d7a87b93c7037c1eea9
ametiste <dara@ametiste.ro>
2015-09-28 11:35:25 +0300

    Conditional configurations

--- metrics-boot/src/main/java/org/ametiste/metrics/boot/configuration/JmxConfiguration.java
+++ metrics-boot/src/main/java/org/ametiste/metrics/boot/configuration/JmxConfiguration.java
@@ -3,13 +3,14 @@ package org.ametiste.metrics.boot.configuration;
 import org.ametiste.metrics.jmx.JmxMetricAggregator;
 import org.ametiste.metrics.MetricsAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(MetricsProperties.class)
-//@ConditionalOnProperty (value = "com.dph.metrics.yammer.disabled", havingValue = "true", matchIfMissing = false)
+@ConditionalOnProperty(prefix="org.ametiste.metrics.yammer", name = "enabled", matchIfMissing = true)
 public class JmxConfiguration {
 
     @Autowired
--- metrics-boot/src/main/java/org/ametiste/metrics/boot/configuration/StatsDConfiguration.java
+++ metrics-boot/src/main/java/org/ametiste/metrics/boot/configuration/StatsDConfiguration.java
@@ -5,12 +5,14 @@ import org.ametiste.metrics.statsd.StatsDMetricAggregator;
 import org.ametiste.metrics.statsd.client.SimpleStatsDClient;
 import org.ametiste.metrics.statsd.client.StatsDClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(MetricsProperties.class)
+@ConditionalOnProperty(prefix="org.ametiste.metrics.statsd", name = "enabled", matchIfMissing = true)
 public class StatsDConfiguration {
 
     @Autowired
