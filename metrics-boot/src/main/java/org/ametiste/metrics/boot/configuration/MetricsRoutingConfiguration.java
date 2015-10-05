4cba5a61cd8cd77dd10fd093273900a4b8cf7612
Konstantin A. Zolotukhin <masted@gmail.com>
2015-10-05 10:13:23 +0300

    + null aggregator is @EnabledAggreagtor now

--- metrics-boot/src/main/java/org/ametiste/metrics/boot/configuration/MetricsRoutingConfiguration.java
+++ metrics-boot/src/main/java/org/ametiste/metrics/boot/configuration/MetricsRoutingConfiguration.java
@@ -32,7 +32,9 @@ public class MetricsRoutingConfiguration {
     }
 
     @Bean
+    @EnabledAggregator
     public MetricsAggregator nullAggregator() {
         return  new NullMetricsAggregator();
     }
+
 }
