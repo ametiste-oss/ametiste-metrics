46f700c3600d806eb5d1e6d291e3c9dacf2c0a3b
Konstantin A. Zolotukhin <masted@gmail.com>
2015-10-25 21:35:12 +0200

    BootMetricServiceAggregatorConfiguration fixed

--- metrics-experimental/src/main/java/org/ametiste/metrics/experimental/boot/BootMetricServiceAggregatorConfiguration.java
+++ metrics-experimental/src/main/java/org/ametiste/metrics/experimental/boot/BootMetricServiceAggregatorConfiguration.java
@@ -4,6 +4,7 @@ import org.ametiste.metrics.MetricsAggregator;
 import org.ametiste.metrics.boot.configuration.CoreAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,10 +18,13 @@ public class BootMetricServiceAggregatorConfiguration {
     @Autowired
     private CounterService counterService;
 
+    @Autowired
+    private GaugeService gaugeService;
+
     @Bean
     @CoreAggregator
     public MetricsAggregator bootMetricServiceAggregator() {
-        return new BootMetricServiceAggregator(counterService);
+        return new BootMetricServiceAggregator(counterService, gaugeService);
     }
 
 }
