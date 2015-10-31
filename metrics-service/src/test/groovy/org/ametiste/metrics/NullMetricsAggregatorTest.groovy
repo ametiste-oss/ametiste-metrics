58cae3dea7c94085dc0e3b6d2ca78ae25b24ec89
ametiste <dara@ametiste.ro>
2015-11-01 01:40:56 +0200

    serivce covered with unit tests

---
+++ metrics-service/src/test/groovy/org/ametiste/metrics/NullMetricsAggregatorTest.groovy
@@ -0,0 +1,30 @@
+package org.ametiste.metrics
+
+import spock.lang.Specification
+
+/**
+ * Created by atlantis on 11/1/15.
+ */
+class NullMetricsAggregatorTest extends Specification {
+
+    NullMetricsAggregator aggregator = new NullMetricsAggregator()
+
+    def gauge() {
+        when: "gauge is called"
+            aggregator.gauge("",4)
+        then: "nothing happens"
+
+    }
+
+    def event() {
+        when: "event is called"
+            aggregator.gauge("",4)
+        then: "nothing happens"
+    }
+
+    def increment() {
+        when: "increment is called"
+            aggregator.gauge("",4)
+        then: "nothing happens"
+    }
+}
