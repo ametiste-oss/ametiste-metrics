22677695cd66504ef69b25ee891b895f180375df
ametiste <dara@ametiste.ro>
2015-11-07 15:55:22 +0200

    trial

---
+++ metrics-filter/src/test/groovy/org/ametiste/metrics/filter/RequestCountFilterTest.groovy
@@ -0,0 +1,21 @@
+package org.ametiste.metrics.filter
+
+import spock.lang.Specification
+
+/**
+ * Created by atlantis on 11/7/15.
+ */
+class RequestCountFilterTest extends Specification {
+    
+    def "Init"() {
+
+    }
+
+    def "DoFilter"() {
+
+    }
+
+    def "Destroy"() {
+
+    }
+}
